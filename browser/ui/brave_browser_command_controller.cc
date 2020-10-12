7a43046047cccdef49416074271526dd6650820f
Simon Hong <shong@brave.com>
2020-10-12 20:46:27 +0900

    Delete unused code

--- browser/ui/brave_browser_command_controller.cc
+++ browser/ui/brave_browser_command_controller.cc
@@ -16,7 +16,6 @@
 #include "brave/components/brave_rewards/browser/buildflags/buildflags.h"
 #include "brave/components/brave_sync/buildflags/buildflags.h"
 #include "brave/components/brave_wallet/buildflags/buildflags.h"
-#include "brave/components/playlist/buildflags/buildflags.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -124,9 +123,6 @@ void BraveBrowserCommandController::InitBraveCommandState() {
 #if BUILDFLAG(ENABLE_TOR)
   UpdateCommandForTor();
 #endif
-#if BUILDFLAG(ENABLE_PLAYLIST)
-  UpdateCommandForPlaylist();
-#endif
   UpdateCommandEnabled(IDC_ADD_NEW_PROFILE, !is_guest_session);
   UpdateCommandEnabled(IDC_OPEN_GUEST_PROFILE, !is_guest_session);
   UpdateCommandEnabled(IDC_TOGGLE_SPEEDREADER, true);
@@ -160,10 +156,6 @@ void BraveBrowserCommandController::UpdateCommandForBraveSync() {
 
 void BraveBrowserCommandController::UpdateCommandForBraveWallet() {
   UpdateCommandEnabled(IDC_SHOW_BRAVE_WALLET, true);
-}
-
-void BraveBrowserCommandController::UpdateCommandForPlaylist() {
-  UpdateCommandEnabled(IDC_SHOW_PLAYLIST, true);
 }
 
 bool BraveBrowserCommandController::ExecuteBraveCommandWithDisposition(
@@ -213,9 +205,6 @@ bool BraveBrowserCommandController::ExecuteBraveCommandWithDisposition(
       break;
     case IDC_SHOW_BRAVE_WALLET:
       brave::ShowBraveWallet(browser_);
-      break;
-    case IDC_SHOW_PLAYLIST:
-      brave::ShowPlaylist(browser_);
       break;
     case IDC_ADD_NEW_PROFILE:
       brave::AddNewProfile();
