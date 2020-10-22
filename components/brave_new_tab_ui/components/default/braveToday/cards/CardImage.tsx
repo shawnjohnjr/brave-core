78230bbac9b56a8d809d24d67b625c6804241553
Pete Miller <miller.pete@gmail.com>
2020-10-22 00:13:18 -0700

    Brave Today: visual fixes

--- components/brave_new_tab_ui/components/default/braveToday/cards/CardImage.tsx
+++ components/brave_new_tab_ui/components/default/braveToday/cards/CardImage.tsx
@@ -8,10 +8,9 @@ import * as Card from '../cardSizes'
 import * as Background from '../../../../../common/Background'
 
 type Props = {
-  size: string
   imageUrl: string
+  list?: boolean
   onLoaded?: () => any
-  fit?: boolean
 }
 
 function useGetUnpaddedImage (paddedUrl: string, onLoaded?: () => any) {
@@ -43,5 +42,10 @@ function useGetUnpaddedImage (paddedUrl: string, onLoaded?: () => any) {
   return unpaddedUrl
 }
 
-  return <Card.Image fit={props.fit} size={props.size} src={data} />
+export default function CardImage (props: Props) {
+  const unpaddedUrl = useGetUnpaddedImage(props.imageUrl, props.onLoaded)
+  const Frame = props.list ? Card.ListImageFrame : Card.ImageFrame
+  return <Frame><Card.Image
+    src={unpaddedUrl}
+  /></Frame>
 }
